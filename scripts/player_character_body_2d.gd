7770d964e3f9942753b8348c4ccee3a74b94e715
Agustin Daniel Palmeira <agustinpalmeira@gmail.com>
2024-10-06 16:40:18 -0300

    KAN-12: Added movement & sprites.

--- scripts/player_character_body_2d.gd
+++ scripts/player_character_body_2d.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
+var is_attacking: bool = false
 
 func _ready():
 	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
@@ -20,6 +21,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
+		
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		self.is_attacking = true
 
 	# Get the input direction and handle the movement/deceleration.
 	var horizontal_direction := Input.get_axis("move_left", "move_right")
@@ -35,19 +39,87 @@ func _physics_process(delta: float) -> void:
 
 
 func update_animations(horizontal_direction):
-	if is_on_floor():
-		if horizontal_direction == 0:
-			ap.play("idle")
+	if !self.is_attacking:
+		if is_on_floor():
+			if horizontal_direction == 0:
+				ap.play("idle")
+			else:
+				ap.play("run")
 		else:
-			ap.play("run")
+			if velocity.y < 0:
+				ap.play("jump")
+			elif velocity.y > 0:
+				ap.play("fall")
 	else:
-		if velocity.y < 0:
-			ap.play("jump")
-		elif velocity.y > 0:
-			ap.play("fall")
-			
+		ap.play("attack")
+		self.is_attacking = false
+	
 func deactivate_camara():
 	$Camera2D.enabled = false
 	
 func add_name(name):
 	$Name.text = name
+
+"""
+extends CharacterBody2D
+
+@export var speed = 300.0
+@export var jump_velocity = -400.0
+
+@onready var ap = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+func _ready():
+	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
+
+#To make it move.
+func _physics_process(delta: float) -> void:
+	var is_attacking: bool = false
+
+	if !$MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		is_attacking = true
+
+	# Get the input direction and handle the movement/deceleration.
+	var horizontal_direction := Input.get_axis("move_left", "move_right")
+	if horizontal_direction:
+		velocity.x = horizontal_direction * speed
+		sprite.flip_h = (horizontal_direction == -1)
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+	
+	update_animations(horizontal_direction, is_attacking)
+
+
+func update_animations(horizontal_direction, is_attacking):
+	if !is_attacking:
+		if is_on_floor():
+			if horizontal_direction == 0:
+				ap.play("idle")
+			else:
+				ap.play("run")
+		else:
+			if velocity.y < 0:
+				ap.play("jump")
+			elif velocity.y > 0:
+				ap.play("fall")
+	else:
+		ap.play("attack")
+	
+func deactivate_camara():
+	$Camera2D.enabled = false
+	
+func add_name(name):
+	$Name.text = name
+"""
