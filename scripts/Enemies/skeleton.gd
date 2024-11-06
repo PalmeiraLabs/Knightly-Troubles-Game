2e34a36af3fd339d6a12576082f51f1f849ea4a7
NicolasAmigo <nicolasamigorey@gmail.com>
2024-11-06 17:38:35 -0300

    refactor: changed enemy state logic

--- scripts/Enemies/skeleton.gd
+++ scripts/Enemies/skeleton.gd
@@ -10,12 +10,11 @@ var health = 10
 @export var max_health = 10
 @export var health_min = 0
 
-var dead: bool = false
-var is_chasing_player: bool = false
-var taking_damage: bool = false
-var is_dealing_damage: bool = false
 var direction : Vector2
-var is_roaming: bool = false
+
+enum State {ROAMING, CHASING, ATTACKING, HURT, DEAD}
+
+var curr_state : State = State.ROAMING
 
 @export var damage_dealt = 1
 @export var knockback = 2
@@ -32,34 +31,40 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if is_dealing_damage:
-		return
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if is_chasing_player and !is_dealing_damage:
-		if position.distance_to(tracked_player.position) < 40:
-			is_dealing_damage = true
-			velocity = Vector2.ZERO
-			
-		else:
-			var dir_to_player = position.direction_to(tracked_player.position)
-			velocity.x = dir_to_player.x * tracking_speed
-	else:
-		velocity.x = direction.x * idle_speed
+	
+	print(curr_state)
+	match curr_state:
+		State.DEAD:
+			return
+		State.ROAMING:
+			print("ACA")
+			velocity.x = direction.x * idle_speed
+		State.ATTACKING:
+			velocity.x = 0
+		State.CHASING:
+			if position.distance_to(tracked_player.position) < 40:
+				curr_state = State.ATTACKING
+				velocity = Vector2.ZERO
+			else:
+				var dir_to_player = position.direction_to(tracked_player.position)
+				velocity.x = dir_to_player.x * tracking_speed
 	handle_animation()
 	move_and_slide()
 
 func _on_direction_timer_timeout():
-	if !is_chasing_player:
+	if curr_state == State.ROAMING:
 		direction = choose([Vector2.LEFT, Vector2.RIGHT, Vector2.ZERO])
 		velocity.x = 0
 		
 func handle_animation():
 	var anim_sprite = $AnimatedSprite2D
-	if !dead and !taking_damage and !is_dealing_damage and velocity == Vector2.ZERO:
-		anim_sprite.play("idle")
-	if !dead and !taking_damage and !is_dealing_damage and velocity != Vector2.ZERO:
-		anim_sprite.play("walk")
+	if curr_state == State.ROAMING or curr_state == State.CHASING:
+		if velocity == Vector2.ZERO:
+			anim_sprite.play("idle")
+		else:
+			anim_sprite.play("walk")
 		if velocity.x < 0 and facing_right:
 			self.scale.x *= -1.0
 			facing_right = false
@@ -67,14 +72,14 @@ func handle_animation():
 			self.scale.x *= -1.0
 			facing_right = true
 	
-	if !dead and is_dealing_damage:
+	if curr_state == State.ATTACKING:
 		anim_sprite.play("attack")
 		$Hitbox/AttackHitbox.disabled = false
 		await get_tree().create_timer(1.5).timeout
 		$Hitbox/AttackHitbox.disabled = true
-		is_dealing_damage = false
+		curr_state = State.CHASING
 	
-	if !dead and taking_damage and !is_dealing_damage:
+	if curr_state == State.HURT:
 		anim_sprite.play("hit")
 		await get_tree().create_timer(0.6).timeout
 	
@@ -83,5 +88,5 @@ func choose(array):
 	return array.front()
 
 func _on_detection_area_body_entered(body):
-	is_chasing_player = true
+	curr_state = State.CHASING
 	tracked_player = body
