cf10f5138f224785f26d7adb47dbe04c1837d8d5
NicolasAmigo <nicolasamigorey@gmail.com>
2024-11-07 13:54:42 -0300

    feat: add hurt logic to first enemy

--- scripts/Enemies/skeleton.gd
+++ scripts/Enemies/skeleton.gd
@@ -8,7 +8,7 @@ const gravity = 900
 
 var health = 10
 @export var max_health = 10
-@export var health_min = 0
+@export var health_min = 1
 
 var direction : Vector2
 
@@ -17,11 +17,13 @@ enum State {ROAMING, CHASING, ATTACKING, HURT, DEAD}
 var curr_state : State = State.ROAMING
 
 @export var damage_dealt = 1
-@export var knockback = 2
 
 var tracked_player : CharacterBody2D = null
 
 var facing_right: bool = true
+
+var in_hurt_animation = false
+var in_death_animation = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -34,14 +36,16 @@ func _process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	print(curr_state)
 	match curr_state:
 		State.DEAD:
 			return
 		State.ROAMING:
-			print("ACA")
 			velocity.x = direction.x * idle_speed
 		State.ATTACKING:
+			velocity.x = 0
+			move_and_slide()
+			return
+		State.HURT:
 			velocity.x = 0
 		State.CHASING:
 			if position.distance_to(tracked_player.position) < 40:
@@ -50,6 +54,7 @@ func _process(delta):
 			else:
 				var dir_to_player = position.direction_to(tracked_player.position)
 				velocity.x = dir_to_player.x * tracking_speed
+				
 	handle_animation()
 	move_and_slide()
 
@@ -59,6 +64,8 @@ func _on_direction_timer_timeout():
 		velocity.x = 0
 		
 func handle_animation():
+	if curr_state == State.DEAD:
+		return
 	var anim_sprite = $AnimatedSprite2D
 	if curr_state == State.ROAMING or curr_state == State.CHASING:
 		if velocity == Vector2.ZERO:
@@ -79,9 +86,26 @@ func handle_animation():
 		$Hitbox/AttackHitbox.disabled = true
 		curr_state = State.CHASING
 	
-	if curr_state == State.HURT:
+	if curr_state == State.HURT and !in_hurt_animation:
+		if health < health_min:
+			curr_state = State.DEAD
+			anim_sprite.play("death")
+			in_hurt_animation = true
+			await get_tree().create_timer(1.4).timeout
+			in_hurt_animation = false
+			self.set_visible(false)
+			self.queue_free()
+		
+		health = health - 1
 		anim_sprite.play("hit")
+		in_hurt_animation = true
 		await get_tree().create_timer(0.6).timeout
+		in_hurt_animation = false
+		if tracked_player == null:
+			#Safeguard. Should not be posible
+			curr_state = State.ROAMING
+		else:
+			curr_state = State.CHASING
 	
 func choose(array):
 	array.shuffle()
@@ -90,3 +114,10 @@ func choose(array):
 func _on_detection_area_body_entered(body):
 	curr_state = State.CHASING
 	tracked_player = body
+
+func _on_hurt_box_body_entered(body):
+	curr_state = State.HURT
+
+
+func _on_hurt_box_area_entered(area):
+	curr_state = State.HURT
