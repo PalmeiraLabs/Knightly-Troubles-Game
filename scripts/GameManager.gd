0d5bb297402501dbc2d6e977e13ebfccdc77a8fa
NicolasAmigo <nicolasamigorey@gmail.com>
2024-11-27 18:01:36 -0300

    feat: add basic level 2

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -5,3 +5,14 @@ extends Node
 
 var players = {}
 var current_player
+
+var ready_to_next_level_players = {}
+
+
+@rpc("any_peer")
+func player_add_ready_next_level(player_id):
+	pass
+	
+@rpc("any_peer")
+func player_remove_ready_next_level(player_id):
+	pass
