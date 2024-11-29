129aab28fe063f90a36d191156975cafc7515ee7
NicolasAmigo <nicolasamigorey@gmail.com>
2024-11-29 14:40:46 -0300

    feat: add level transitions

--- scenes/level_transition.gd
+++ scenes/level_transition.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+signal level_compleated(level_name)
+@export var next_level: PackedScene
+var players_ready = {}
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_area_2d_body_entered(body):
+	add_name.rpc(body.name)
 
+@rpc("any_peer", "call_local")
+func add_name(player_name):
+	players_ready[player_name] = null
+	if players_ready.size() == GameManager.players.size():
+		level_compleated.emit(next_level.get_path())
+		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@rpc("any_peer", "call_local")
+func remove_name(player_name):
+	players_ready.erase(player_name)
+
+func _on_area_2d_body_exited(body):
+	add_name.rpc(body.name)
