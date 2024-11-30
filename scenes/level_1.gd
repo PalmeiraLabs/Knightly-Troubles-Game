22f5a032b44b1cbd8bfd751003bbe87260a106fd
NicolasAmigo <72048568+NicolasAmigo@users.noreply.github.com>
2024-11-30 14:01:10 -0300

    Feature/feat 47/add level transition (#30)
    * feat: add level transition scene
    
    * feat: add basic level 2
    
    * feat: add level transitions

--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal level_compleated(level_name)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -7,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_level_transition_level_compleated(next_level):
+	level_compleated.emit(next_level)
--- scripts/SceneManager.gd
+++ scripts/SceneManager.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var PlayerScene : PackedScene
 
+signal level_compleated(level_name)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_player_spawns()
@@ -20,4 +22,29 @@ func set_player_spawns():
 		for spawn in get_tree().get_nodes_in_group("PlayerSpawnPoint"):
 			if spawn.name == str(index):
 				current_player.global_position = spawn.global_position
+		index += 1
+		
+
+
+
+func _on_level_level_compleated(level_name):
+	change_level(level_name)
+	
+	
+func change_level(next_level_name):
+	# Removes previous level
+	remove_child($Level)
+	
+	# Creates next level in memory
+	var next_level = load(next_level_name).instantiate()
+	next_level.name = "Level"
+	add_child(next_level)
+
+	# Moves players to the start of the level
+	var index = 0
+	for i in GameManager.players:
+		var curr_player = get_node("%s" % i)
+		for spawn in get_tree().get_nodes_in_group("PlayerSpawnPoint"):
+			if spawn.name == str(index):
+				curr_player.global_position = spawn.global_position
 		index += 1
