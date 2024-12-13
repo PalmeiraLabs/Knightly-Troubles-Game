33caab14c11cf2d331cb2beefda915a103949822
Agustin Daniel Palmeira <agustinpalmeira@gmail.com>
2024-12-13 20:10:03 -0300

    KAN-92: workaround for showing the winning scene.

--- scripts/Levels/level_2.gd
+++ scripts/Levels/level_2.gd
@@ -1,4 +1,4 @@
-extends Node2D
+class_name Level2 extends Node2D
 
 const GAME_ENDING_SCENE = "res://scenes/Menus/game_ending.tscn"
 signal level_compleated(level_name)
@@ -11,15 +11,29 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_level_transition_level_compleated(next_level):
 	print("DEBUG: level 2 completed...")
+	
+	next_level = GAME_ENDING_SCENE
+	
 	if next_level == GAME_ENDING_SCENE:
-		self.addScene(GAME_ENDING_SCENE)
+		print("DEBUG: next_level == GAME_ENDING_SCENE in Level 2...")
+		self.replace_all_scenes(GAME_ENDING_SCENE)
 	else:
+		print("DEBUG: next_level != GAME_ENDING_SCENE in Level 2...")
 		level_compleated.emit(next_level)
 
 func addScene(sceneName):
+	print("DEBUG: Adding scene... ", sceneName)
 	var scene = load(sceneName).instantiate()
 	get_tree().root.add_child(scene)
 	self.hide()
+
+func replace_all_scenes(new_scene_path: String):
+	# Remove all existing scenes
+	var root = get_tree().root
+	for child in root.get_children():
+		child.queue_free()
+	# Add the new scene
+	var new_scene = load(new_scene_path).instantiate()
+	root.add_child(new_scene)
--- scripts/Levels/level_transition.gd
+++ scripts/Levels/level_transition.gd
@@ -9,13 +9,19 @@ func _on_area_2d_body_entered(body):
 
 @rpc("any_peer", "call_local")
 func add_name(player_name):
+	print("DEBUG: add_name in LevelTransition...")
+	
+	# Ensure GameManager is valid
+	if GameManager == null or GameManager.players == null:
+		print("ERROR: GameManager or players dictionary is null!")
+		return
+	
 	players_ready[player_name] = null
 	if players_ready.size() == GameManager.players.size():
 		print("DEBUG: Level transition...")
 		print("DEBUG: next level path: ", next_level.get_path())
 		level_compleated.emit(next_level.get_path())
 		
-
 @rpc("any_peer", "call_local")
 func remove_name(player_name):
 	players_ready.erase(player_name)
